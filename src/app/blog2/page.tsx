1b5893e72c7c2ccf8f1394435238911778215258
kiwi334334 <99292273+kiwi334334@users.noreply.github.com>
2023-08-15 19:55:44 +0000

    modified:   .gitignore 	new file:   src/app/blog1/page.tsx 	new file:   src/app/blog2/layout.tsx 	new file:   src/app/blog2/page.tsx 	modified:   src/app/globals.css 	modified:   src/app/page.tsx

---
+++ src/app/blog2/page.tsx
@@ -0,0 +1,9 @@
+export default function page() {
+    return (
+        <>
+            <div className="w-screen h-screen bg-gradient-to-l from-emerald-500 to-teal-300">
+
+            </div>
+        </>
+    ) 
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,40 @@
-export default function signin(){
+"use client"
+
+
+import Link from "next/link"
+import React from "react";
+
+export default function signin(){  
+  function Linkcheck() {
+    if(name === "EW" && pass === "980039480"){
+      return(
+        <>
+          <Link href="./blog1">Elias's Blog</Link>
+        </>
+      )
+    }
+    if(name === "GIANNA" && pass === "IsTheBest"){
+      return(
+        <>
+          <Link href="/blog2">Gianna blog</Link>
+        </>
+      )
+    }
+  }
+  const [name, setName] = React.useState('');
+  const [pass, setPass] = React.useState('');
+
   return(
     <>
       <div className="w-screen h-screen bg-green-300">
         <div className="bg-slate-100 rounded absolute left-[30%] top-[30%] w-40% h-40% ">
           <form>
             <h1>Sign In</h1>
-            <input placeholder="type username here"/>
-            <input placeholder="type password here"/>
+            <input value={name} onChange={(event) => {setName(event.target.value);}} placeholder="type username here"/>
+            <input value={pass} onChange={(event) => {setPass(event.target.value);}} placeholder="type password here"/>
+            <br/>
           </form>
+          <Linkcheck/>
         </div>
       </div>
     </>
